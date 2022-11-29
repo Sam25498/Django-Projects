b3bce7648dd51e42ff3b7c97abbcfef43168ea6e
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-11-29 11:43:31 +0300

    Update views.py

--- rship/rapport/views.py
+++ rship/rapport/views.py
@@ -6,7 +6,7 @@ from .models import Description
 # Create your views here.
 class IndexView(generic.ListView):
     template_name = 'rapport/index.html'
-    context_object_name = 'description_list'
+    context_object_name = 'latest_description_list'
     
     def get_queryset(self):
         """Return the last five keyed entries"""
