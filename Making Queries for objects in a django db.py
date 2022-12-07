bf2e239ee3866ee7b438589999820ab1132cc395
Byegon <71707603+Sam25498@users.noreply.github.com>
2022-12-07 13:16:59 +0300

    Update Making Queries for objects in a django db.py

--- Making Queries for objects in a django db.py
+++ Making Queries for objects in a django db.py
@@ -2,18 +2,18 @@
   
   from rapport.models import Description
 
-In [3]: Description.objects.all()
-Out[3]: <QuerySet [<Description: Emmanuel's Birthday>, <Description: Ridely's Birthday>, <Description: Caleb's birthday>, <Description: Sam's Birthday>, <Description: My mum's Birthday>]>
+>>>  Description.objects.all()
+>>>  <QuerySet [<Description: Emmanuel's Birthday>, <Description: Ridely's Birthday>, <Description: Caleb's birthday>, <Description: Sam's Birthday>, <Description: My mum's Birthday>]>
 
-In [4]: Description.objects.filter(id=1)
-Out[4]: <QuerySet [<Description: Emmanuel's Birthday>]>
+>>>  Description.objects.filter(id=1)
+>>>  <QuerySet [<Description: Emmanuel's Birthday>]>
 
-In [5]: all_entries = Description.objects.all()
+>>>  all_entries = Description.objects.all()
 
-In [6]: all_entries
-Out[6]: <QuerySet [<Description: Emmanuel's Birthday>, <Description: Ridely's Birthday>, <Description: Caleb's birthday>, <Description: Sam's Birthday>, <Description: My mum's Birthday>]>
+>>>  all_entries
+>>>  <QuerySet [<Description: Emmanuel's Birthday>, <Description: Ridely's Birthday>, <Description: Caleb's birthday>, <Description: Sam's Birthday>, <Description: My mum's Birthday>]>
 
-In [7]: one_entry = Entry.objects.get(pk=1)
+>>>  one_entry = Entry.objects.get(pk=1)
 ---------------------------------------------------------------------------
 NameError                                 Traceback (most recent call last)
 <ipython-input-7-3569ec73d039> in <cell line: 1>()
@@ -21,18 +21,38 @@ NameError                                 Traceback (most recent call last)
 
 NameError: name 'Entry' is not defined
 
-In [8]: one_entry = Description.objects.get(pk=1)
+>>>  one_entry = Description.objects.get(pk=1)
 
-In [9]: one_entry
-Out[9]: <Description: Emmanuel's Birthday>
+>>>  one_entry
+>>>  <Description: Emmanuel's Birthday>
 
-In [10]: Description.objects.all()[:5]
-Out[10]: <QuerySet [<Description: Emmanuel's Birthday>, <Description: Ridely's Birthday>, <Description: Caleb's birthday>, <Description: Sam's Birthday>, <Description: My mum's Birthday>]>
+>>>  Description.objects.all()[:5]
+>>>  <QuerySet [<Description: Emmanuel's Birthday>, <Description: Ridely's Birthday>, <Description: Caleb's birthday>, <Description: Sam's Birthday>, <Description: My mum's Birthday>]>
 
-In [11]: Description.objects.all().values('description_text')
-Out[11]: <QuerySet [{'description_text': "Info of my bro's birthday."}, {'description_text': "Info on my friend's birthday."}, {'description_text': "Info of my cousin's birthday."}, {'description_text': 'Info of my birthday.'}, {'description_text': "Info on my mum's birthday."}]>
+>>>  Description.objects.all().values('description_text')
+>>>  <QuerySet [{'description_text': "Info of my bro's birthday."}, {'description_text': "Info on my friend's birthday."}, {'description_text': "Info of my cousin's birthday."}, {'description_text': 'Info of my birthday.'}, {'description_text': "Info on my mum's birthday."}]>
 
  
 
-In [12]: Description.objects.all().values_list('description_text')
-Out[12]: <QuerySet [("Info of my bro's birthday.",), ("Info on my friend's birthday.",), ("Info of my cousin's birthday.",), ('Info of my birthday.',), ("Info on my mum's birthday.",)]>                   
+>>> Description.objects.all().values_list('description_text')
+>>> <QuerySet [("Info of my bro's birthday.",), ("Info on my friend's birthday.",), ("Info of my cousin's birthday.",), ('Info of my birthday.',), ("Info on my mum's birthday.",)]>                   
+12
+
+
+To get the list of usernames:
+
+>>> User.objects.all().values('username')
+>>> [{'username': u'u1'}, {'username': u'u2'}]
+
+>>> User.objects.all().values_list('username')
+>>> [(u'u1',), (u'u2',)]
+If you want just strings, a list comprehension can do the trick:
+
+>>> usr_names = User.objects.all().values('username')
+>>> [u['username'] for u in usr_names]
+>>> [u'u1', u'u2']
+Using values_list:
+
+>>> usr_names = User.objects.all().values_list('username')
+>>> [u[0] for u in usr_names]
+>>> [u'u1', u'u2']
