5a3273b072a0c06f3c28bbcdda60066aec9793d9
Byegon <71707603+Sam25498@users.noreply.github.com>
2024-01-05 23:25:12 +0300

    Update Views.py

--- Data viz app/Dviz/Views.py
+++ Data viz app/Dviz/Views.py
@@ -16,3 +16,22 @@ def visualize_data(request):
             columns = list(data[0].keys())
             values = {col: [row[col] for row in data] for col in columns}
 
+            for col, values_list in values.items():
+                plt.bar(range(len(values_list)), values_list, label=col)
+
+            plt.title('User-generated Bar Chart')
+            plt.xlabel('X-axis')
+            plt.ylabel('Y-axis')
+            plt.legend()
+            
+            chart_path = 'dataviz/static/dataviz/chart.png'
+            plt.savefig(chart_path)  # Save chart as a static file
+
+            # Save user input data
+            form.save()
+
+            return render(request, 'dataviz/visualization.html', {'chart_path': chart_path})
+    else:
+        form = DataForm()
+
+    return render(request, 'dataviz/index.html', {'form': form})
