6b3eb8a5da2aff6ac5ccc9d12a6cc4e82d2a2675
Byegon <71707603+Sam25498@users.noreply.github.com>
2024-01-07 21:29:03 +0300

    Update Views.py

--- chart_website/chart_app/Views.py
+++ chart_website/chart_app/Views.py
@@ -5,4 +5,16 @@ from .models import ChartData
 from .utils import generate_chart
 from django.template import loader
 
+def index(request):
+    latest_charts_list = ChartData.objects.order_by('-pub_date')[:5]
+    template = loader.get_template('chart_app/index.html')
+    context = {
+        'latest_charts_list': latest_charts_list,
+    }
+    return HttpResponse(template.render(context, request))
+    
+
+
+
+
 
