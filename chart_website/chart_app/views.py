6f1152e67b2a2b50eeb2ae1225bce348d7891aef
Byegon <71707603+Sam25498@users.noreply.github.com>
2024-02-07 12:04:45 +0300

    Update views.py

--- chart_website/chart_app/views.py
+++ chart_website/chart_app/views.py
@@ -24,5 +24,57 @@ class IndexView(generic.ListView):
         return ChartData.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
  
+class ChartDetailView(generic.DetailView):
+    model = ChartData
+    template_name = 'chart_app/chart_detail.html'
+    context_object_name = 'chart_data'
+    slug_field = 'data_title'
+    slug_url_kwarg = 'data_title'
+
+   
+from django.shortcuts import render, redirect
+from django.views import View
+from .models import ChartData
+from .forms import ChartForm
+
+class AddChartView(View):
+    template_name = 'chart_app/add_chart.html'
+
+    def get(self, request):
+        form = ChartForm()
+        return render(request, self.template_name, {'form': form})
+
+    def post(self, request):
+        form = ChartForm(request.POST)
+
+        if form.is_valid():
+            # Create a new ChartData instance
+            chart_data = ChartData(
+                data_title=form.cleaned_data['data_title'],
+                data_type=form.cleaned_data['data_type'],
+                chart_type=form.cleaned_data['chart_type'],
+            )
+
+            # Example data dictionary
+            data_dict = {
+                'name': 'John',
+                'Axis Titles': ['Time', 'Price'],
+                'X': ['Standard Class', 'First Class', 'Second Class', 'Same Day'],
+                'Y': [3200, 3000, 2800, 1000],
+                'xlabel': 'Principal Component', 
+                'ylabel': 'Cumulative Explained Variance', 
+                'color': 'blue'
+            }
+
+            # Use the save_data method to save the data
+            chart_data.save_data(data_dict)
+
+            return redirect('chart_app:index')
+
+        return render(request, self.template_name, {'form': form})
+
+    
+    
+
 
 
