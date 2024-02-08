a3150a57a6a751b12e1f6808552de65fb2704640
Byegon <71707603+Sam25498@users.noreply.github.com>
2024-02-08 17:06:13 +0300

    Update models.py

--- chart_website/chart_app/models.py
+++ chart_website/chart_app/models.py
@@ -2,3 +2,16 @@ import json
 from django.db import models
 from django.utils import timezone
 
+class ChartData(models.Model):    
+    data_title = models.CharField(max_length=50)
+    data_type = models.CharField(max_length=50)
+    # Use CharField with choices for chart_type
+    CHART_TYPE_CHOICES = [
+        ('Line Chart', 'Line Chart'),
+        ('Bar Chart', 'Bar Chart'),
+        ('Pie Chart', 'Pie Chart'),
+        ('Donut Chart', 'Donut Chart'),
+        ('Bar and Line Chart', 'Bar and Line Chart'),
+        ('Horizontal Bar Chart','Horizontal Bar Chart'),
+    ]
+    
